46755b7f2995998270375618cb874c33a6d3090c
alexisjanvier <contact@alexisjanvier.net>
2016-02-08 22:17:54 +0100

    Fix Babel preset

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 import '../css/main.css';
 import city from './paris';
 
-const text = 'For the moment, you are in ' + city;
-
-document.getElementById('start_here').innerHTML = text;
+document.getElementById('start_here').innerHTML = `For the moment, you are in ${city}`;
